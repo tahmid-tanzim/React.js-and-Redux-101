7cde0cd6f75b9c16523827a8c268fa7f7ac2aa8a
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-11-12 14:18:16 +0600

    Building the Node.js server app

---
+++ README.md
@@ -0,0 +1,13 @@
+# Polling App
+Build with Socket IO and React.js
+### 1. Building the Node.js server app
+```
+cd /<path-to>/polling-app
+npm install express
+npm install bootstarp
+```
+### Run server
+```
+node app-server.js
+```
+
---
+++ app-server.js
@@ -0,0 +1,11 @@
+var express = require('express'),
+    app = express();
+
+/**
+ * Note: Middleware app.use will serve everything static from public directory and bootstrap.
+ * */
+app.use(express.static('./public'));
+app.use(express.static('./node_modules/bootstrap/dist'));
+
+app.listen(3000);
+console.log("Polling server is running at 'http://localhost:3000'");
